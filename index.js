b0db2cb30d97bf5837de4d72188c238ad63a7071
Morgan Zolob <mzolob@gmail.com>
2017-02-04 20:51:01 -0800

    Update mysql code for GoJS and to use database connection info
    This changes the mysql code to use POST with the database connection
    info as the body of the request. It also changes the get data function
    to return database schema.
    
    The JSON returned by both SQL functions is also now much more friendly
    for GoJS, so the processing we have to do on it is very minimal.
    
    
    Former-commit-id: f32d73df394da06774ed1fe41f8e2f98fc4a650b
    Former-commit-id: 97f4da1453a0a0234a5326482c44374b667b4400

--- index.js
+++ index.js
@@ -2,13 +2,18 @@
 
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 var mysqlQueries = require('./mysql-queries.js');
 
 app.use(express.static('static'));
 
-app.get('/sql/get', mysqlQueries.getDataAction);
-app.get('/sql/relations', mysqlQueries.getRelationsAction);
+app.post('/sql/schema', mysqlQueries.getSchemaAction);
+app.post('/sql/relations', mysqlQueries.getRelationsAction);
 
 // Redirect any other paths to angular
 app.all(/\/.*/, function(req, res) {
--- mysql-queries.js
+++ mysql-queries.js
@@ -9,78 +9,115 @@ var pool = mysql.createPool({
     debug: true
 });
 
-module.exports.getDataAction = function(req, res) {
+function sendError(message, errObject, res)
+{
+    console.log("SQL Error: " + message);
+    console.log(errObject);
+    res.json({
+        "code": 100,
+        "status": message
+    });
+}
 
-    pool.getConnection(function (err, connection) {
+function performQuery(connectionParams, query, errorCallback, callback)
+{
+    // Connection is defined by post variables
+    var connection = mysql.createConnection({
+        host: connectionParams.host,
+        port: connectionParams.port,
+        user: connectionParams.user,
+        password: connectionParams.password,
+        database: connectionParams.database
+    });
+
+    connection.connect(function (err) {
         if (err) {
-            connection.release();
-            res.json({
-                "code": 100,
-                "status": "Error in connection database"
-            });
+            connection.end();
+            errorCallback("Error connecting to database", err);
             return;
         }
 
-        console.log('connected as id ' + connection.threadId);
+        console.log('Connected as id ' + connection.threadId);
 
-        connection.query('select * from ' + req.query.table, function (err, rows) {
-            connection.release();
-            if (!err) {
-                res.json(rows);
-            } else {
-                res.json({
-                    "code": 101,
-                    "status": err
-                });
+        connection.query(query, function (err, rows) {
+            connection.end();
+
+            if (err)
+            {
+                errorCallback("Error executing query", err);
+                return;
             }
+
+            callback(rows);
         });
 
         connection.on('error', function (err) {
-            res.json({
-                "code": 100,
-                "status": "Error in connecting database"
-            });
+            connection.end();
+            errorCallback("Database connection error", err);
         });
     });
+}
+
+module.exports.getSchemaAction = function(req, res) {
+
+    // This is a magical sql query that I crafted. It's pretty cool
+    var sqlQuery = 'SELECT c.TABLE_NAME AS "table", c.COLUMN_NAME AS "column", ' +
+        'SUM(CASE WHEN tc.CONSTRAINT_TYPE = "PRIMARY KEY" THEN 1 ELSE 0 END) AS "primary", ' +
+        'SUM(CASE WHEN tc.CONSTRAINT_TYPE = "FOREIGN KEY" THEN 1 ELSE 0 END) AS "foreign" ' +
+        'FROM information_schema.`COLUMNS` AS c ' +
+        'LEFT JOIN information_schema.KEY_COLUMN_USAGE AS kcu ON kcu.TABLE_SCHEMA = c.TABLE_SCHEMA ' +
+        'AND kcu.TABLE_NAME = c.TABLE_NAME AND kcu.COLUMN_NAME = c.COLUMN_NAME ' +
+        'LEFT JOIN information_schema.TABLE_CONSTRAINTS AS tc ON tc.TABLE_SCHEMA = c.TABLE_SCHEMA ' +
+        'AND tc.TABLE_NAME = c.TABLE_NAME AND tc.CONSTRAINT_NAME = kcu.CONSTRAINT_NAME ' +
+        'WHERE c.TABLE_SCHEMA = "' + pool.config.connectionConfig.database + '" ' +
+        'GROUP BY c.TABLE_NAME, c.COLUMN_NAME ORDER BY c.TABLE_NAME, c.COLUMN_NAME';
+
+    performQuery(req.body, sqlQuery,
+        function(m, e) {
+            sendError(m, e, res);
+        },
+        function(rows) {
+            // Build our json object for go.js
+            var tables = [];
+
+            while (rows.length > 0)
+            {
+                var row = rows.shift();
+
+                // This looks gross. Thank closures.
+                var table = tables.find((function(row) {
+                    return function (e) {
+                        if (e.key === row.table)
+                            return true;
+                    }
+                })(row));
+
+                if (table === undefined)
+                {
+                    table = {key: row.table, items: []};
+                    tables.push(table);
+                }
+
+                table.items.push({
+                    name: row.column,
+                    iskey: row.primary || row.foreign,
+                    figure: row.primary || row.foreign ? "Decision" : "Cube1",
+                    color: row.primary && row.foreign ? "PrimaryForeign" : row.primary ? "Primary" : row.foreign ? "Foreign" : "None"
+                })
+            }
+
+            res.json(tables);
+        }
+    );
 };
 
 module.exports.getRelationsAction = function(req, res) {
 
-    pool.getConnection(function (err, connection) {
-        if (err) {
-            connection.release();
-            res.json({
-                "code": 100,
-                "status": "Error in connection database"
-            });
-            return;
-        }
+    var sqlQuery = 'SELECT kcu.TABLE_NAME as "from", kcu.REFERENCED_TABLE_NAME as "to", "0..N" as "text", "1" as "toText" ' +
+        'FROM information_schema.TABLE_CONSTRAINTS AS tc ' +
+        'JOIN information_schema.KEY_COLUMN_USAGE AS kcu ON tc.CONSTRAINT_NAME = kcu.CONSTRAINT_NAME ' +
+        'WHERE tc.TABLE_SCHEMA = "' + pool.config.connectionConfig.database + '" AND tc.CONSTRAINT_TYPE = "FOREIGN KEY"';
 
-        console.log('connected as id ' + connection.threadId);
-
-        var sqlquery = 'SELECT kcu.TABLE_NAME as manyTable, kcu.COLUMN_NAME as manyColumn, kcu.REFERENCED_TABLE_NAME as oneTable, kcu.REFERENCED_COLUMN_NAME as oneColumn ' +
-                       'FROM information_schema.TABLE_CONSTRAINTS AS tc ' +
-                       'JOIN information_schema.KEY_COLUMN_USAGE AS kcu ON tc.CONSTRAINT_NAME = kcu.CONSTRAINT_NAME ' +
-                       'WHERE tc.TABLE_SCHEMA = "' + pool.config.connectionConfig.database + '" AND tc.CONSTRAINT_TYPE = "FOREIGN KEY"';
-
-        connection.query(sqlquery, function (err, rows) {
-            connection.release();
-            if (!err) {
-                res.json(rows);
-            } else {
-                res.json({
-                    "code": 101,
-                    "status": "Query returned an error: " + err
-                });
-            }
-        });
-
-        connection.on('error', function (err) {
-            res.json({
-                "code": 100,
-                "status": "Error in connecting database"
-            });
-        });
-    });
-
+    performQuery(req.body, sqlQuery, function(m, e) { sendError(m, e, res); }, res.json);
+    
 };
