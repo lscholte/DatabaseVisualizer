4b6b434f949a2b51b1dbf5428292d5fdb3abf5f4
Ben lockwood <benlock96@gmail.com>
2017-03-16 15:59:19 -0700

    DROP-59 : Added functionality to select files in the add project interface along with created the file to parse the inputed code

---
+++ static/js/parse_code.js
@@ -0,0 +1,10 @@
+document.getElementById("filepicker").addEventListener("change", function(event) {
+  let output = document.getElementById("listing");
+  let files = event.target.files;
+
+  for (let i=0; i<files.length; i++) {
+    let item = document.createElement("li");
+    item.innerHTML = files[i].webkitRelativePath;
+    output.appendChild(item);
+  };
+}, false);
